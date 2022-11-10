e7248686722f4f297bdc3db64fa41189be6bb638
kaylee125 <kaylee125@naver.com>
2022-11-10 13:09:19 +0900

    20221110 recommit

--- hospital_rest/accounts/urls.py
+++ hospital_rest/accounts/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name='accounts'
 
 urlpatterns = [
     path('signup/', views.signup,name='signup'),
-    path('login/', views.signin ,name='login'),
-    path('logout/', views.logout,name='logout'),
+    # path('login/', views.signin ,name='login'),
+    # path('logout/', views.logout,name='logout'),
+    path('login/',  auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile,name='profile'),
 
 
